1dc36ee84d4e25d82aa8b90bd13fa008294637f5
Niapoll <kolya21082001@gmail.com>
2022-06-07 23:41:46 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Voronova
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
